fffd9934154ba101779be5bb691cec496ee4d4b6
Louis Gesbert <louis.gesbert@hotmail.com>
2021-07-04 11:37:38 +0200

    Restaurant Model

---
+++ db/migrate/20210704093708_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :adress
+      t.integer :stars
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_07_04_093708) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "adress"
+    t.integer "stars"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
